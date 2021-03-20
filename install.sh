a72e3e2d9bb408c6d8de43d16073101b6c96d5bd
lamhai1401 <lamthanhhai141@gmail.com>
2021-03-20 17:59:57 +0700

    init

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ cargo install cargo-update
 cargo clean
 cargo update
 cargo build --release
+cargo build --target # x86_64-unknown-linux-musl x86_64-unknown-linux-gnu stable-x86_64-apple-darwin
+
+rustup show # We can use to show us the installed toolchains
+rustup update # and to keep them up to date with Rust's releases.
+rustup target add  x86_64-unknown-linux-musl x86_64-unknown-linux-gnu stable-x86_64-apple-darwin
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,2 +1,18 @@
-fn main() {
+use std::io::{self, Write};
+
+use futures::sink::SinkExt;
+use futures::stream::StreamExt;
+use structopt::StructOpt;
+use url::Url;
+use websocket_lite::{ClientBuilder, Message, Opcode, Result};
+
+const CONNECTION: &'static str = "wss://signal-conference-staging.quickom.com";
+
+// fn main() {
+//     println!("Connecting to {}", CONNECTION);
+// }
+
+#[tokio::main]
+async fn main() {
+    println!("Hello async")
 }
